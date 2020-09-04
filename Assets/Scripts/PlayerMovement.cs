e4c6ac3f957c911bcd66e7b6f44f8252b62694dc
LAPTOP-CUB9DE1T\youc <1807814469@qq.com>
2020-09-04 22:27:02 +0800

    人物初始位置延时记录

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,10 @@ public class PlayerMovement : MonoBehaviour
     private Animator anim;
     private Rigidbody2D rb;
 
+    [Header("记录人物位置延迟时间")]
+    public float time;
+    private bool pT;
+
     [Header("人物上下移动距离")]
     public float Y;
     [Header("切换赛道速度")]
@@ -18,8 +22,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //记录初始位置
-        v = this.transform.position;
+        
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
@@ -27,6 +30,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (pT)
+        {
+            Invoke("Wait", time);
+        }
+        
         //移动
         if (Input.GetKeyDown(KeyCode.A))
         {
@@ -45,5 +53,12 @@ public class PlayerMovement : MonoBehaviour
             }
         }
         transform.position = Vector3.MoveTowards(transform.position, dest, move);
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        //记录初始位置
+        v = transform.position;
+        pT = false;
     }
 }
