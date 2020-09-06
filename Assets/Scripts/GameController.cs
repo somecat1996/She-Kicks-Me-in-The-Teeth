55ab8a71b268c3571acadd104b3f78ec9b5ca546
Lynn Wang <369150573@qq.com>
2020-09-06 23:54:52 +0800

    地图初步完成
    地图的产生和销毁
    地图加速

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [Header("初始速度")]
+    public float velocity;
+    [Header("加速度")]
+    public float acceleration;
+    [Header("新跑道创建x坐标")]
+    public float createPosition;
+    [Header("跑道列表")]
+    public GameObject[] tracks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("ZeroTrack").GetComponent<TrackBehavior>().OnInitial(velocity, acceleration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity += acceleration * Time.deltaTime;
+    }
+
+    public void OnCreateTrack()
+    {
+        GameObject newTrack = GameObject.Instantiate(RandomSelect(), new Vector3(createPosition, 0, 0), Quaternion.identity);
+        newTrack.GetComponent<TrackBehavior>().OnInitial(velocity, acceleration);
+    }
+
+    private GameObject RandomSelect()
+    {
+        int num = Random.Range(0, tracks.Length);
+        return tracks[num];
+    }
+}
---
+++ Assets/Scripts/TrackBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackBehavior : MonoBehaviour
+{
+    public int leftRange;
+    public int rightRange;
+
+    private float velocity;
+    private float accleration;
+    private Rigidbody2D rb;
+    private GameController gm;
+
+    private bool haveCreated = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity += accleration * Time.deltaTime;
+        rb.velocity = Vector2.left * velocity;
+
+        if (transform.position.x <= leftRange)
+        {
+            Destroy(this.gameObject);
+        }
+        else if (transform.position.x <= rightRange && !haveCreated)
+        {
+            gm.OnCreateTrack();
+            haveCreated = true;
+        }
+    }
+
+    public void OnInitial(float v, float a)
+    {
+        velocity = v;
+        accleration = a;
+        rb.velocity = Vector2.left * velocity;
+    }
+}
