323336b5534565aa74d19c6a0b03b513615e6ef9
Lynn Wang <369150573@qq.com>
2020-09-09 23:18:52 +0800

    镜头震动

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    private Cinemachine.CinemachineCollisionImpulseSource MyInpulse;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MyInpulse = GetComponent<Cinemachine.CinemachineCollisionImpulseSource>();
     }
 
     // Update is called once per frame
@@ -20,6 +21,7 @@ public class EnemyBehavior : MonoBehaviour
     {
         if (collision.tag == "Attack")
         {
+            MyInpulse.GenerateImpulse();
             Destroy(gameObject);
         }
     }
