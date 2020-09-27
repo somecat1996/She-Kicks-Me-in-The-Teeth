fa50cf660571f2ff50855664a73798102a03d5b7
LAPTOP-CUB9DE1T\youc <1807814469@qq.com>
2020-09-27 19:55:42 +0800

    朋克婴儿实现

---
+++ Assets/Scripts/Capacity/Car.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public bool drive;
+
+    public float CD;
+    private float startCD;
+
+    public float last;
+    private float startLast;
+
+    private void Start()
+    {
+        drive = false;
+        startCD = CD;
+
+        startLast = last;
+    }
+    //技能冷却计算
+    private void Update()
+    {
+        if (!drive&&CD>=0)
+        {
+            CD -= Time.deltaTime;
+        }
+        else if (CD<0)
+        {
+            drive = true;
+            CD = startCD;
+        }
+    }
+    //开车冲刺
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Block"&&drive && last >= 0)
+        {
+            Destroy(collision.gameObject);
+            last -= Time.deltaTime;
+        }
+        else if(collision.tag == "Block" && !drive && last < 0)
+        {
+            last = startLast;
+        }
+    }
+}
